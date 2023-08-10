14eb55f887408d552ec007221cc2437a77baee90
m-ole0 <138345360+m-ole0@users.noreply.github.com>
2023-08-11 04:04:32 +0900

    [Finish]応用課題7c

--- config/routes.rb
+++ config/routes.rb
@@ -16,6 +16,8 @@ Rails.application.routes.draw do
     get 'followers' => 'relationships#followers', as: 'followers'
   end
 
+  resources :groups, only:[:new, :create, :index, :show, :edit, :update, :destroy]
+
   get "search", to: "searches#search"
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
